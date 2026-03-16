f6ee020fe5e62a23f92d972bb79c32378af804b6
ThomasSeery <tommyboy4050@outlook.com>
2026-03-16 22:14:18 +0000

    namespace refactor

---
+++ src/Concertable.AppHost/DistributedApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+internal static class DistributedApplicationBuilderExtensions
+{
+    public static IResourceBuilder<SqlServerDatabaseResource> AddSqlServer(this IDistributedApplicationBuilder builder)
+    {
+        return builder.AddSqlServer("sql")
+                      .WithDataVolume("concertable-sql-data")
+                      .AddDatabase("DefaultConnection");
+    }
+
+    public static IResourceBuilder<ProjectResource> AddApi(this IDistributedApplicationBuilder builder, IResourceBuilder<SqlServerDatabaseResource> sql)
+    {
+        return builder.AddProject<Projects.Concertable_Web>("api")
+                      .WithReference(sql)
+                      .WaitFor(sql);
+    }
+
+    public static IResourceBuilder<NodeAppResource> AddFrontend(this IDistributedApplicationBuilder builder, IResourceBuilder<ProjectResource> api)
+    {
+        return builder.AddNpmApp("frontend", "../../ClientApp", "start")
+                      .WithHttpEndpoint(port: 4200, isProxied: false)
+                      .WithReference(api)
+                      .WaitFor(api);
+    }
+}
